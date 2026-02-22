67c2ea3eed07e5f50c5c2f6efbb001790e5be44b
123ramanathan <ngksriram23111@gmail.com>
2026-02-22 22:34:46 +0530

    changes on 22-2-2026

--- src/app/pages/barcode-scan/barcode-scan.page.ts
+++ src/app/pages/barcode-scan/barcode-scan.page.ts
@@ -4,6 +4,7 @@ import {
   BarcodeFormat
 } from '@capacitor-mlkit/barcode-scanning';
 import { NavController } from '@ionic/angular';
+import { Db } from 'src/app/service/db';
 
 @Component({
   selector: 'app-barcode-scan',
@@ -13,28 +14,34 @@ import { NavController } from '@ionic/angular';
 })
 export class BarcodeScanPage implements OnDestroy {
 
-  scannedValue: any = [
-    {
-      book_name: 'Sriram'
-    },
-    {
-      book_name: 'John'
-    },
-    {
-      book_name: 'Rohan'
-    },
-    {
-      book_name: 'Sudalai'
-    },
-    {
-      book_name: 'Doss'
-    },
-  ];
+  // scannedValue: any = [
+  //   {
+  //     book_name: 'Sriram'
+  //   },
+  //   {
+  //     book_name: 'John'
+  //   },
+  //   {
+  //     book_name: 'Rohan'
+  //   },
+  //   {
+  //     book_name: 'Sudalai'
+  //   },
+  //   {
+  //     book_name: 'Doss'
+  //   },
+  // ];
+  scannedValue: any = [];
 
-  constructor(private navCtrl: NavController) {}
+  constructor(private navCtrl: NavController, public db: Db) {}
 
   async ionViewDidEnter() {
     // await this.startScan();
+  }
+
+  ionViewWillEnter(){
+    // this.scannedValue = [];
+    // this.getScannedBookDetails("Perfect Question Perfect Answer Tamil");
   }
 
   async startScan() {
@@ -55,15 +62,33 @@ export class BarcodeScanPage implements OnDestroy {
     });
 
     if (result.barcodes.length > 0) {
-      let data = {
-        book_name: result.barcodes[0].rawValue
-      }
-      this.scannedValue.push(data);
-      console.log('Scanned:', this.scannedValue);
-
-      // navigate back or process result
-      // this.navCtrl.back();
+      // let data = {
+      //   book_name: result.barcodes[0].rawValue
+      // }
+      // this.scannedValue.push(data);
+      console.log('Scanned:', result.barcodes[0].rawValue);
+      this.getScannedBookDetails(result.barcodes[0].rawValue);
     }
+  }
+
+  getScannedBookDetails(bookName: any) {
+    let data = {
+      "user": localStorage['user_id'],
+      "search_book_name": "",
+      "language": "",
+      "item_group": "All Categories",
+      "start": 1,
+      "page_length": 10,
+      "scan_bar_code": bookName
+    }
+    this.db.sales_items_with_filters(data).subscribe((res : any) => {
+      // console.log(res, "getScannedBookDetails");
+      if(res && res.message && res.message.items && res.message.items.length > 0 && res.status == 'Success'){
+        this.scannedValue.push(...res.message.items);
+      }else{
+        this.db.presentToast("No details found for the scanned book", "danger");
+      }
+    })
   }
 
   ionViewWillLeave() {
